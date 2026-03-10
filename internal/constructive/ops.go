d6bd0cd547839b5bf041c21f42c5a1054596252e
shlaapnik <artem.tema1000@gmail.com>
2026-03-11 00:16:47 +0300

    feat: add basic operations for constructive real numbers

---
+++ internal/constructive/ops.go
@@ -0,0 +1,60 @@
+﻿package constructive
+
+func Add(a, b Real) Real {
+	return NewFromApprox(func(eps float64) (float64, error) {
+		if eps <= 0 {
+			return 0, ErrInvalidEpsilon
+		}
+
+		aApprox, err := a.Approx(eps / 2)
+		if err != nil {
+			return 0, err
+		}
+
+		bApprox, err := b.Approx(eps / 2)
+		if err != nil {
+			return 0, err
+		}
+
+		return aApprox + bApprox, nil
+	})
+}
+
+func Sub(a, b Real) Real {
+	return NewFromApprox(func(eps float64) (float64, error) {
+		if eps <= 0 {
+			return 0, ErrInvalidEpsilon
+		}
+
+		aApprox, err := a.Approx(eps / 2)
+		if err != nil {
+			return 0, err
+		}
+
+		bApprox, err := b.Approx(eps / 2)
+		if err != nil {
+			return 0, err
+		}
+
+		return aApprox - bApprox, nil
+	})
+}
+func Mul(a, b Real) Real {
+	return NewFromApprox(func(eps float64) (float64, error) {
+		if eps <= 0 {
+			return 0, ErrInvalidEpsilon
+		}
+
+		aApprox, err := a.Approx(eps / 4)
+		if err != nil {
+			return 0, err
+		}
+
+		bApprox, err := b.Approx(eps / 4)
+		if err != nil {
+			return 0, err
+		}
+
+		return aApprox * bApprox, nil
+	})
+}
