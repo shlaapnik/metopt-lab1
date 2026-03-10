26cb20fc7f9adb368237dc07b4e77eeaa9626307
shlaapnik <artem.tema1000@gmail.com>
2026-03-11 00:26:30 +0300

    feat: add comparison for constructive real numbers

---
+++ internal/constructive/compare.go
@@ -0,0 +1,28 @@
+﻿package constructive
+
+import "math"
+
+func Compare(a, b Real, eps float64) (int, error) {
+	if eps <= 0 {
+		return 0, ErrInvalidEpsilon
+	}
+
+	aApprox, err := a.Approx(eps / 2)
+	if err != nil {
+		return 0, err
+	}
+
+	bApprox, err := b.Approx(eps / 2)
+	if err != nil {
+		return 0, err
+	}
+
+	diff := aApprox - bApprox
+	if math.Abs(diff) <= eps {
+		return 0, nil
+	}
+	if diff < 0 {
+		return -1, nil
+	}
+	return 1, nil
+}
