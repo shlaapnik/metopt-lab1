a76cfad3c91a2e664738eb69123cb0c2e17b9fc1
1_Sherbakov_Artem <artemsherbackov15@gmail.com>
2026-03-11 19:35:53 +0300

    feat: add utils for work with rand and add clone func

--- internal/genetic/utils.go
+++ internal/genetic/utils.go
@@ -1,1 +1,24 @@
 package genetic
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func RandomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func CloneGenes(genes []float64) []float64 {
+	d := make([]float64, len(genes))
+	copy(d, genes)
+	return d
+}
